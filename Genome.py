f1c9354d7882e95bbc61b93ba3570e6443d824c9
Titouan <tpoquillon@gmail.com>
2019-04-05 09:23:17 +0200

    Change name from AI to Genome
    Signed-off-by: Titouan <tpoquillon@gmail.com>

--- Genome.py
+++ Genome.py
@@ -7,7 +7,7 @@ Created on Fri Apr  5 08:23:06 2019
 """
 import numpy as np
 
-class AI:
+class Genome:
   def __init__(self,I,O):  #Default Contructor
     
     self.O=O  #range of the Output vector
@@ -25,10 +25,12 @@ class AI:
     self.H=len(Matrix)-self.I
     self.Hiden=np.zeros((1,self.H))
     self.Map=Matrix[:,:]
+
     
+
 if __name__ == '__main__':
-  g1=AI(3,10)
-  print(g1.Map==np.zeros((3,10)), g1.O==10, g1.I==3,g1.H==0, g1.Hiden==np.array([]))
-  g2=AI(2,2)
-  g2.Copy_Map(np.array([[1, 2], [3, 4]]))
-  print(g2.Map==np.array([[1, 2], [3, 4]]))
+  gm1=Genome(3,10)
+  print(gm1.Map==np.zeros((3,10)), gm1.O==10, gm1.I==3,gm1.H==0, gm1.Hiden==np.array([]))
+  gm2=Genome(2,2)
+  gm2.Copy_Map(np.array([[1, 2], [3, 4]]))
+  print(gm2.Map==np.array([[1, 2], [3, 4]]))

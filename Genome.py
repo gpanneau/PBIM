76be7241ba226e098f41866c6ecc4873199f356a
Titouan <tpoquillon@gmail.com>
2019-04-05 09:40:38 +0200

    Methode Processing + minor changes
    Processing(self,Input):  #Product vector of decision based from
    a vector of action passed as a parameter
    
    add _ at the end of parameters
    Change Copy_Map to set Map
    
    Signed-off-by: Titouan <tpoquillon@gmail.com>

--- Genome.py
+++ Genome.py
@@ -10,27 +10,36 @@ import numpy as np
 class Genome:
   def __init__(self,I,O):  #Default Contructor
     
-    self.O=O  #range of the Output vector
+    self.O_=O  #range of the Output vector
     
-    self.I=I  #range of the Input vector
+    self.I_=I  #range of the Input vector
     
-    self.H=0  #range of the Hiden vector
+    self.H_=0  #range of the Hiden vector
     
-    self.Hiden=np.array([])  #Hiden vector
+    self.Hiden_=np.array([])  #Hiden vector
     
-    self.Map=np.zeros((I,O))  #Conection Matrix
+    self.Map_=np.zeros((I,O))  #Conection Matrix
 
 
-  def Copy_Map(self,Matrix):  #Matrix Seter by copy
-    self.H=len(Matrix)-self.I
-    self.Hiden=np.zeros((1,self.H))
-    self.Map=Matrix[:,:]
+  def Set_Map(self,Matrix):  #Matrix Seter by copy
+    self.H_=len(Matrix)-self.I_
+    self.Hiden_=np.zeros((1,self.H_))
+    self.Map_=Matrix[:,:]
 
     
+
+  def Processing(self,Input):  #Product vector of decision based from a vector of action passed as a parameter
+    Out=(np.concatenate((Input,self.Hiden_),axis=None).dot(self.Map_))>0  
+    self.Hiden_=1*Out[self.O_:self.O_+self.H_]
+    return Out[0:self.O_]
 
 if __name__ == '__main__':
   gm1=Genome(3,10)
-  print(gm1.Map==np.zeros((3,10)), gm1.O==10, gm1.I==3,gm1.H==0, gm1.Hiden==np.array([]))
+  print(gm1.Map_==np.zeros((3,10)), gm1.O_==10, gm1.I_==3,gm1.H_==0, gm1.Hiden_==np.array([]))
   gm2=Genome(2,2)
-  gm2.Copy_Map(np.array([[1, 2], [3, 4]]))
-  print(gm2.Map==np.array([[1, 2], [3, 4]]))
+  gm2.Set_Map(np.array([[1, 2], [3, 4]]))
+  print(gm2.Map_==np.array([[1, 2], [3, 4]]))
+  gm3=Genome(2,4)
+  gm3.Set_Map(np.array([[0,1,0,1],[1,0,1,0]]))
+  print(gm3.Processing(np.array([1,0]))==np.array([False,True,False,True]))
+  

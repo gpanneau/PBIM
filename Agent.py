b7b6dc513afd3707498c90edc619780bc4e81c0d
Claire Lemonnier <claire.lemonnier@insa-lyon.fr>
2019-04-05 10:03:56 +0200

    Commmentary modified in Agent.py
    Signed-off-by: Claire Lemonnier <claire.lemonnier@insa-lyon.fr>

--- Agent.py
+++ Agent.py
@@ -10,7 +10,7 @@ import numpy as np
 import Genome
 
 class Agent:
-  def __init__(self,x,y,myGenome,grid):#Copy constructor whitch takes a genome, a grid  and a position on this grid
+  def __init__(self,x,y,myGenome,grid):#Constructor whitch takes an existing genome, a grid  and a position wanted for the agent on this grid
     self.posX_=x
     self.posY_=y
     self.Genome_= Genome.Genome(myGenome.I_,myGenome.O_)

5f807bd95f35e8c476b1062faa003d81504ed518
Claire Lemonnier <claire.lemonnier@insa-lyon.fr>
2019-04-30 14:59:49 +0200

    Correction error in Agent Class
    Environment grid was not copied (donc il y avait un effet de bord quand
    on modifiait la matrice d'environnement)
    
    Signed-off-by: Claire Lemonnier <claire.lemonnier@insa-lyon.fr>

--- Agent.py
+++ Agent.py
@@ -15,7 +15,7 @@ class Agent:
     self.posY_=y
     self.Genome_= Genome.Genome(myGenome.I_,myGenome.O_)
     self.Genome_.Set_Map(myGenome.Map_)
-    self.Environment_=grid
+    self.Environment_=np.matrix.copy(grid)
     self.decision_=[False,False,False]
     
     
@@ -26,8 +26,12 @@ class Agent:
     
 if __name__ == '__main__':
   mat1=np.zeros((5,5))
-  ai1=Genome.Genome(3,10)
+  genom1=Genome.Genome(3,10)
   a=0
   b=0
-  ag1=Agent(a,b,ai1,mat1)
-  print(ag1.posX_==a,ag1.posY_==b,ag1.Genome_==ai1,ag1.Environment_==mat1,ag1.decision_==[0,0,0])
+  ag1=Agent(a,b,genom1,mat1)
+  mat1[0,0]=3
+  print(ag1.posX_==a,ag1.posY_==b,ag1.Genome_==genom1,ag1.Environment_,mat1,ag1.decision_==[0,0,0])
+  print(ag1.Genome_)
+  print(genom1)
+  

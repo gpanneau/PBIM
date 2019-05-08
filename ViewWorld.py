7e2104365b5c295249592a043d170e4321c94354
Armande Cirot <armande.cirot@insa-lyon.fr>
2019-05-08 10:43:10 +0200

    Bobby est un oeil, et il a une grille carrée ^^
    Signed-off-by: Armande Cirot <armande.cirot@insa-lyon.fr>

--- ViewWorld.py
+++ ViewWorld.py
@@ -2,17 +2,25 @@ import tkinter as tk
 
 class CreateWorld(tk.Canvas):
 	def __init__(self, fenetre):
-		tk.Canvas.__init__(self, width = 1000, height = 1000, highlightthickness=0,bg="green")
+		tk.Canvas.__init__(self, width = 1000, height = 300, highlightthickness=0,bg="green")
 		self.size_in_pixels = 1000
-    
+		#self.img = tk.PhotoImage(file="papillon.png")
+		
 	def draw_grid(self, Mygrid):
 		self.w = len(Mygrid[0])
 		self.h = len(Mygrid)
-		self.pixels_height = int(self.size_in_pixels)/self.h
+		self.pixels_height = int(self.size_in_pixels)/self.w
 		self.pixels_width = int(self.size_in_pixels)/self.w
+		#self.pixels_height = int(self.size_in_pixels)/self.h
+		#self.pixels_width = int(self.size_in_pixels)/self.w
 		for i in range(self.h):
 		    for j in range(self.w):
 		        if (Mygrid[i][j]==0):
 		            self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue")
 		        elif (Mygrid[i][j]==2):
-		            self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="red")
+		            #imglabel = tk.Label(image=self.img)
+		            self.create_oval(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="cyan")
+		            self.create_oval((0.5+j)*self.pixels_width,(i+0.2)*self.pixels_height, (j+1-0.2)*self.pixels_width,(i+1-0.4)*self.pixels_height,fill="white")
+		            self.create_oval((0.65+j)*self.pixels_width,(i+0.3)*self.pixels_height, (j+1-0.25)*self.pixels_width,(i+1-0.6)*self.pixels_height,fill="black")
+		            
+		
